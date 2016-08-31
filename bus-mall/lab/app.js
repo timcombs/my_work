45dda4987a57fe25d845a8cf4da754cdc90de6b8
Tim Combs <tim00combs@gmail.com>
2016-08-30 17:02:50 -0700

    add clicks & views incrementing

--- bus-mall/lab/app.js
+++ bus-mall/lab/app.js
@@ -15,10 +15,18 @@ for (var i = 0; i < imagePaths.length; i++) {
   //pushing null because will push something else into it later
   new Image(name, path, null);
 }
-//display first 3 images
-for (var j = 0; j < 3; j++) {
-  drawImage();
-}
+
+//add prototypes to Image constructor
+Image.prototype.addClick = function() {
+  this.clicks += 1;
+};
+Image.prototype.addView = function() {
+  this.views += 1;
+};
+
+//generate indices for 1st 3 images & display images
+var arrayOfRandomIndices = randomIndices();
+drawImage(arrayOfRandomIndices);
 
 //get element from html to append images
 var imageList = document.getElementById('images');
@@ -26,44 +34,69 @@ var imageList = document.getElementById('images');
 imageList.addEventListener('click', clickHandler);
 
 function clickHandler(e) {
-  //use event target to determine which image was clicked
-  //add to views of all images displayed
-  //add to the clicks of just the clicked image
+  //get name of clicked image using event target
+  var target = e.target.src;
 
-  //clear images before displaying new images
-  imageList.textContent = '';
-  for (var k = 0; k < 3; k++) {
-    drawImage();
-  }  
-  console.log('click', e.target);
+  if (target) {
+    //clear images before displaying new images
+    imageList.textContent = '';
+
+    target = target.slice(61, e.target.length);
+    var targetName = target.substring(0, target.indexOf("."));
+
+    //increment click property on image object
+    for (var m = 0; m < images.length; m++) {
+      if (images[m].name === targetName) {
+        images[m].addClick();
+      }
+    }
+
+    //display 3 separate images
+    var arrayOfRandomIndices = randomIndices();
+    drawImage(arrayOfRandomIndices);
+  }
 }
 
 //make this draw out all 3 images - loops
-//make this check index of images for no repeats - loops
-//use the image path for the source
-function drawImage() {
-  //create elements to attach to DOM
-  var img = document.createElement('img');
-  var li = document.createElement('li');
+function drawImage(array) {
+  for (var q = 0; q < array.length; q++) {
+    //create elements to attach to DOM
+    var img = document.createElement('img');
+    var li = document.createElement('li');
 
-  //get element to attach images to
-  var imageList = document.getElementById('images');
+    //get element to attach images to
+    var imageList = document.getElementById('images');
 
-  //get random number between 0 & length of imagePaths array to use as index
-  var randomIndex = Math.floor(Math.random() * imagePaths.length);
+    //set src using each index from array
+    img.setAttribute('src', 'img/' + imagePaths[array[q]]);
 
-  //set src
-  img.setAttribute('src', 'img/' + imagePaths[randomIndex]);
+    //add to DOM
+    li.appendChild(img);
+    imageList.appendChild(li);
 
-  //add to DOM
-  li.appendChild(img);
-  imageList.appendChild(li);
-  //console.log(img.src);
+    //increment view property on image object
+    //object in the image array at the index at index p of the passed in array
+    var theObjectIndex = array[q];
+    images[theObjectIndex].addView();
+  }
+}
+
+function randomIndices() {
+  var firstRandomIndex = Math.floor(Math.random() * imagePaths.length);
+  var secondRandomIndex = Math.floor(Math.random() * imagePaths.length);
+  while (firstRandomIndex === secondRandomIndex) {
+    var secondRandomIndex = Math.floor(Math.random() * imagePaths.length);
+  }
+  var thirdRandomIndex = Math.floor(Math.random() * imagePaths.length);
+  while (firstRandomIndex === thirdRandomIndex || secondRandomIndex === thirdRandomIndex) {
+    var thirdRandomIndex = Math.floor(Math.random() * imagePaths.length);
+  }
+  return [firstRandomIndex, secondRandomIndex, thirdRandomIndex];
 }
 
 //image object constructor
 function Image(name, path) {
-  this.view = 0;
+  this.views = 0;
   this.clicks = 0;
   this.name = name;
   this.path = path;
